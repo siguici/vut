adc22b35d9b7df848f194aa21ef1a392c025b6af
Sigui Kessé Emmanuel <contact@sigui.ci>
2023-12-27 05:06:59 +0100

    :closed_lock_with_key: Add assertion structures for all primitive types

--- src/expectations.v
+++ src/expectations.v
@@ -5,25 +5,105 @@ struct Expectation[E] {
 	expected E
 }
 
+@[params]
+struct BoolExpectation {
+	Expectation[bool]
+}
+
+@[params]
+struct StringExpectation {
+	Expectation[string]
+}
+
+@[params]
+struct RuneExpectation {
+	Expectation[rune]
+}
+
+@[params]
+struct I8Expectation {
+	Expectation[i8]
+}
+
+@[params]
+struct U8Expectation {
+	Expectation[u8]
+}
+
+@[params]
+struct I16Expectation {
+	Expectation[i16]
+}
+
+@[params]
+struct U16Expectation {
+	Expectation[u16]
+}
+
+@[params]
+struct IntExpectation {
+	Expectation[int]
+}
+
+@[params]
+struct U32Expectation {
+	Expectation[u32]
+}
+
+@[params]
+struct I64Expectation {
+	Expectation[i64]
+}
+
+@[params]
+struct U64Expectation {
+	Expectation[u64]
+}
+
+@[params]
+struct F32Expectation {
+	Expectation[f32]
+}
+
+@[params]
+struct F64Expectation {
+	Expectation[f64]
+}
+
+@[params]
+struct ISizeExpectation {
+	Expectation[isize]
+}
+
+@[params]
+struct USizeExpectation {
+	Expectation[usize]
+}
+
+@[params]
+struct VoidptrExpectation {
+	Expectation[voidptr]
+}
+
 pub fn expect[E](expected E) Expectation[E] {
 	return Expectation[E]{expected}
 }
 
-pub fn expect_bool(expected bool) Expectation[bool] {
-	return Expectation{expected}
+pub fn expect_bool(expected bool) BoolExpectation {
+	return BoolExpectation{expected}
 }
 
-pub fn (expectation Expectation[voidptr]) to_be_nil() Expectation[voidptr] {
+pub fn (expectation VoidptrExpectation) to_be_nil() VoidptrExpectation {
 	assert_nil(expectation.expected)
 	return expectation
 }
 
-pub fn (expectation Expectation[bool]) to_be_true() Expectation[bool] {
+pub fn (expectation BoolExpectation) to_be_true() BoolExpectation {
 	assert_true(expectation.expected)
 	return expectation
 }
 
-pub fn (expectation Expectation[bool]) to_be_false() Expectation[bool] {
+pub fn (expectation BoolExpectation) to_be_false() BoolExpectation {
 	assert_false(expectation.expected)
 	return expectation
 }
