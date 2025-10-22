710ded603f72cfbda6a39a8b52522382a4a76a0a
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-10-22 15:03:09 +0100

    🚧 Add `is_bool` rule

--- src/validation/rule.v
+++ src/validation/rule.v
@@ -12,3 +12,16 @@ pub struct Rule {
 pub fn new_rule(name string, apply RuleFn, message string, is_validation bool) Rule {
 	return Rule{name, apply, message, is_validation}
 }
+
+pub fn is_bool() Rule {
+	return Rule{
+		name:    'is_bool'
+		message: 'Expected a boolean'
+		check:   fn (val any) ? {
+			if typeof(val).name == 'bool' {
+				return
+			}
+			return error('Expected a boolean')
+		}
+	}
+}
